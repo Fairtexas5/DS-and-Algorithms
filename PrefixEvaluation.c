e5eaa6d35f97a9b799feca9118ef3cb1aeb6f432
Fairtexas5 <114144412+Fairtexas5@users.noreply.github.com>
2023-05-28 16:28:34 +0530

    Update PrefixEvaluation.c
    Correct code for evaluating Prefix expression using queue and Multi digit numbers can also be added in expression and operators and numbers need to be separated by space.

--- PrefixEvaluation.c
+++ PrefixEvaluation.c
@@ -1,166 +1,156 @@
 #include <stdio.h>
-#include<string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <math.h>
 
-int array2[50],front=-1,rear=-1,i,x,j=1,operand1,operand2,result,result1=0,count=0;
-char queue[50],exp[50];
+#define MAXLINE 500
+typedef struct node
+{
+    char data[MAXLINE];
+    struct node *link;
+} NODE;
+typedef struct queue
+{
+    NODE *front;
+    NODE *rear;
+    int count;
+} QUEUE;
 
-void enqueuechar(char c);
-void check(char c1,char c2,char c3);
-int operation(char op,int operand1,int operand2);
-void enqueueint(int integer);
-void dequeue();
+QUEUE* CreateQueue(){
+    QUEUE* q = (QUEUE*)malloc(sizeof(QUEUE));  
+    q->front = NULL;                           
+    q->rear = NULL;                            
+    q->count = 0;                              
+    return q;
+}
+void Enqueue(QUEUE *q, char* dataIn){
+    NODE *newNode = (NODE*)malloc(sizeof(NODE));   
+    strcpy(newNode->data, dataIn);                 
+    newNode->link = NULL;
+    if (q->front == NULL)
+        q->front = newNode;                        
+    else
+        q->rear->link = newNode;
+    q->rear = newNode;
+    //printf(" New node is %s", dataIn);
+    q->count++;                            
+}
+
+void Dequeue(QUEUE *q, char *dataOut){
+    NODE *temp = q->front;
+    strcpy(dataOut, q->front->data);    
+    if (q->count == 1)                  
+        q->rear = NULL;                 
+    q->front = q->front->link;          
+    q->count--;                         
+    free(temp);
+}
+int QueueCount(QUEUE *q){
+    return q->count;         
+}
+
+void Display(QUEUE *q){
+    NODE *loc = q->front; // first node
+    printf("\n");
+    printf("expression: ");
+    while (loc != NULL)
+    {
+        printf("%s ", loc->data);
+        loc = loc->link;
+    }    
+}
+
+void DeleteQueue(QUEUE *q){
+    NODE *temp;
+    while (q->front != NULL)
+    {
+        temp = q->front;
+        q->front = q->front->link;
+        free(temp);
+    }
+    free(q);
+}
+
+int calculate(char a, int b, int c){
+     
+    if(a=='+')
+        return (b+c);
+     
+    else if(a=='-')
+        return (b-c);
+    else if(a=='*')
+        return (b*c);
+    else if(a=='/')
+        return (b/c);
+    else if (a=='^')
+        return (pow(b,c));
+    else if (a=='%')
+        return (b%c);
+    else
+        return -1;
+}
+
+int calculateExpression(QUEUE *q){
+
+    char data[MAXLINE], dt1[MAXLINE],dt2[MAXLINE],t_opr, t_op1, t_op2, *dptr;
+    int i, operand1, operand2, value;
+
+    while ((QueueCount(q)!=1)){
+        Dequeue(q, data);
+        t_opr=data[0];
+        if(ispunct(t_opr)){// if t_opr is punctuation '+' or '_' or '*' or '/'
+                dptr=q->front->data;//data pointer is pointing to the 2nd data in the queue
+                t_op1=*dptr;
+                strcpy(dt1,dptr); // copies the string pointed by dptr to dt1
+             
+                dptr=q->front->link->data;
+                t_op2=*dptr;      //data pointer is poiting to the 3rd data in the queue
+                strcpy(dt2,dptr); // copies the string pointed by dptr to dt2
+                if(!ispunct(t_op1)&&!ispunct(t_op2)){
+                        operand1= atoi (dt1);      
+                        operand2= atoi (dt2);       
+                        Dequeue(q, dt1);   
+                        Dequeue(q, dt2);   
+                        value=calculate(t_opr,operand1, operand2);
+                        printf (" \n\n Caculation : %d %c %d = %d\n",operand1,t_opr,operand2,value);
+                        // itoa (value, data, 10);      
+                        sprintf(data,"%d",value);
+                        dptr=data;
+                        Enqueue(q, dptr);  
+                        Display(q);
+                    }
+                    else{
+                        dptr=data;                   
+                        Enqueue(q, dptr);   
+                        Display(q);
+                    }
+            }
+            else{
+                dptr=data;
+                Enqueue(q, dptr); 
+                Display(q);
+            }     
+    }
+    Dequeue(q, data);
+    return atoi(data); 
+}
 
 int main(){
-  printf("\nEnter the Prefix Expression:");
-  scanf("%s",exp);
-  
-  for(i=0;exp[i]!='\0';i++){
-    enqueuechar(exp[i]);
-  }
-  queue[i]='\0';
-  
-  for(i=0;queue[i]!='\0';i++) {
-     count++;  
-  }  
- while(count!=1){
-    rear=-1;
-    front=-1;
-    for(i=0;queue[i]!='\0';i++){ 
-      check(queue[i],queue[i+1],queue[i+2]);
-      
-      if(x==1){
-        enqueuechar(queue[i]);
-      }
-        else{
-        operand1=array2[i+1];
-        operand2=array2[i+2];
-        
-        result=operation(queue[i],operand1,operand2);
-        
-        enqueueint(result); 
-        
-        i=i+2;
-      }   
-      
+    char expr[MAXLINE];
+    //Use spaces to differentiate between operators and numbers
+    printf("Enter expression: ");
+    fgets(expr, MAXLINE, stdin);
+    //"- + * 2 3 * 5 4 10" = "16";
+    char *token;
+    int finalvalue;
+    QUEUE *q = CreateQueue();   
+    token = expr;
+    while ((token = strtok(token, " "))){
+        Enqueue(q, token);
+        token = NULL;
     }
-    enqueuechar('\0');
-    printf("\nAfter %d scan,we have\n",j);
-    j++;
-    count=0; 
-    for(i=0;queue[i]!='\0';i++) {
-     dequeue();
-     count++;  
-    }  
-    printf("\n");
-  }
-  printf("\nResult of Prefix Expression:%d\n",array2[0]);
-  return 0;
-}
-
-
-void enqueuechar(char c){
-  if(rear==49)
-     printf("\nQueue Overflow");
-  else{
-     if(front==-1 && rear==-1){
-       front=front+1;
-       rear=rear+1;
-     }
-     
-     else{
-       rear=rear+1;
-     }
-     
-     queue[rear]=c;
-     if(queue[rear]!='+' && queue[rear]!='-' && queue[rear]!='*' && queue[rear]!='/' && queue[rear]!='\0'){ 
-        array2[rear]=c-48;       
-      }
-   }
-}
-
-void enqueueint(int integer){
-  if(rear==49)
-     printf("\nQueue Overflow");
-  else{
-     if(front==-1 && rear==-1){
-       front=front+1;
-       rear=rear+1;
-     }
-     
-     else{
-       rear=rear+1;
-     }
-     
-     queue[rear]=integer;
-     if(queue[rear]!='+' && queue[rear]!='-' && queue[rear]!='*' && queue[rear]!='/' && queue[rear]!='\0'){ 
-        array2[rear]=integer;       
-      }
-   }
-}
-
-void dequeue(){
-  if(front==-1)
-     printf("\nQueue Underflow");
-  else{
-     if(queue[front]!='+' && queue[front]!='-' && queue[front]!='*' && queue[front]!='/' && queue[front]!='\0'){ 
-        printf("%d\t",array2[front]);       
-      }
-      else{
-        printf("%c\t",queue[front]);
-      }
-     front+=1;
-     if(front>rear){
-       front=-1;
-       rear=-1;
-     }
-    }
-}
-
-void check(char c1,char c2,char c3){
-  x=0;
-  
-  if(c2=='\0')
-     x=1;
-    
-     
-  if(c1!='+' && c1!='-' && c1!='*' && c1!='/') 
-     x= 1;
-     
-  else{
-    if(c2=='+' || c2=='-'|| c2=='*'|| c2=='/')
-       x=1;
-       
-    else{
-      if(c3=='+' || c3=='-'|| c3=='*'|| c3=='/')
-         x=1;
-    }
-  }  
-  
-}
-
-
-int operation(char op,int operand1,int operand2){
-  switch(op){
-    case '+':
-             result1=operand1+operand2;
-             break;
-             
-    case '-':
-             result1=operand1-operand2;
-             break;   
-             
-    case '*':
-             result1=operand1*operand2;
-             break;
-             
-    case '/':
-             result1=operand1/operand2;
-             break;  
-             
-    default:
-            printf("\nOperation cannot be performed");                     
-             
-  }
-  
-  return result1;
+    finalvalue=calculateExpression(q);
+    printf("\n\n value of the expression %d", finalvalue);    
+    DeleteQueue(q);  
 }
